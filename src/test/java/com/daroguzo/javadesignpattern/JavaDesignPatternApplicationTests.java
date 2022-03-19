148ab7d43ba25ef927f5c36d7a1add05ec2f32b1
daroguzo <kjw2122@naver.com>
2022-03-20 01:33:48 +0900

    Initial Commit

---
+++ src/test/java/com/daroguzo/javadesignpattern/JavaDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daroguzo.javadesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
