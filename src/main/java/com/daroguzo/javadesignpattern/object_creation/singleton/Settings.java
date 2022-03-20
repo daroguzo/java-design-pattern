b79066e498afa0e1c87d074d01d7ebe4ff94aa17
daroguzo <kjw2122@naver.com>
2022-03-20 22:42:35 +0900

    singleton pattern2 멀티 쓰레드 환경 해결책 3 double checked locking(효율적인 동기화 블록 만들기)

--- src/main/java/com/daroguzo/javadesignpattern/object_creation/singleton/Settings.java
+++ src/main/java/com/daroguzo/javadesignpattern/object_creation/singleton/Settings.java
@@ -2,12 +2,20 @@ package com.daroguzo.javadesignpattern.object_creation.singleton;
 
 public class Settings {
 
-    private static final Settings INSTANCE = new Settings();
+    private static volatile Settings instance;
 
     private Settings() {}
 
     public static Settings getInstance() {
-        return INSTANCE;
+        if (instance == null) {
+            synchronized (Settings.class) {
+                if (instance == null) {
+                    instance = new Settings();
+                }
+            }
+        }
+
+        return instance;
     }
 
 }
