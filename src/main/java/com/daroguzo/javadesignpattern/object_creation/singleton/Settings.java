f143eb095d6f3b8bb17fb99c0e9298099d1ab050
daroguzo <kjw2122@naver.com>
2022-03-20 22:54:42 +0900

    singleton pattern singleton 패턴을 깨트리는 방법2(직렬화&역직렬화)

--- src/main/java/com/daroguzo/javadesignpattern/object_creation/singleton/Settings.java
+++ src/main/java/com/daroguzo/javadesignpattern/object_creation/singleton/Settings.java
@@ -1,6 +1,8 @@
 package com.daroguzo.javadesignpattern.object_creation.singleton;
 
-public class Settings {
+import java.io.Serializable;
+
+public class Settings implements Serializable {
 
     private Settings() {}
 
