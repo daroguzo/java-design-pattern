3f3f450a6bd60bb53a0d871e1c19105e39582374
daroguzo <kjw2122@naver.com>
2022-04-17 17:13:21 +0900

    add: decorator pattern

---
+++ src/main/java/com/daroguzo/javadesignpattern/structure/decorator/_01_before/CommentService.java
@@ -0,0 +1,7 @@
+package com.daroguzo.javadesignpattern.structure.decorator._01_before;
+
+public class CommentService {
+    public void addComment(String comment) {
+        System.out.println(comment);
+    }
+}
---
+++ src/main/java/com/daroguzo/javadesignpattern/structure/decorator/_02_after/CommentService.java
@@ -0,0 +1,6 @@
+package com.daroguzo.javadesignpattern.structure.decorator._02_after;
+
+public interface CommentService {
+
+    void addComment(String comment);
+}
