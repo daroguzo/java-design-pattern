3e6dd031a27f3811dd9c5fd01f3cbd4dab3a60c1
daroguzo <kjw2122@naver.com>
2022-04-17 18:33:41 +0900

    add: facade pattern

---
+++ src/main/java/com/daroguzo/javadesignpattern/structure/facade/_02_after/EmailSettings.java
@@ -0,0 +1,14 @@
+package com.daroguzo.javadesignpattern.structure.facade._02_after;
+
+public class EmailSettings {
+
+    private String host;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
